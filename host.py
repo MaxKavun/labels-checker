8a54808c862ca878d1f8cf59dffe10cb89d4e734
Maksim Kavun <maksim.kavun@intexsoft.by>
2019-10-07 16:14:15 +0300

    Finish webhook

--- host.py
+++ host.py
@@ -1,9 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 
 app = Flask(__name__)
 
-@app.route("/administer")
-def hello():
-	return "Hello World"
+@app.route("/", methods=['POST'])
+def webhook():
+        allowed = False
+        request_info = request.json
+        for x in request_info['request']['object']['spec']['containers']:
+                if "nginx" in x['image']:
+                        allowed = True
+                        break
+        admission_response = {
+                "allowed": allowed,
+                "uid": request_info['request']['uid']
+        }
+        admissionReview = {
+                "apiVersion": "admission.k8s.io/v1",
+                "kind": "AdmissionReview",
+                "response": admission_response
+        }
+        return jsonify(admissionReview)
 app.run(host="0.0.0.0")
